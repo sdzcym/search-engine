b5d3a0c26242fa04836a4bc3726f0d940d26fbe2
Erica M Yuan <59790872+sdwfym@users.noreply.github.com>
2020-11-18 22:00:57 -0800

    Create RetrievalModelUnrankedBoolean.java

---
+++ retrieval_model/RetrievalModelUnrankedBoolean.java
@@ -0,0 +1,17 @@
+package retrieval_model;
+/**
+ *  Copyright (c) 2019 Carnegie Mellon University.  All Rights Reserved.
+ */
+
+/**
+ *  An object that stores parameters for the unranked Boolean
+ *  retrieval model (there are none) and indicates to the query
+ *  operators how the query should be evaluated.
+ */
+public class RetrievalModelUnrankedBoolean extends RetrievalModel {
+
+  public String defaultQrySopName () {
+    return new String ("#or");
+  }
+
+}
